236a644c9fbfce6ac1d54a7f90ab491fac41e381
mkidee <mckaymemmott@gmail.com>
2023-08-03 18:26:37 -0600

    thought controller - create and delete reactions

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -55,5 +55,26 @@ module.exports = {
                 )
                 .catch((err) => res.status(500).json(err));
     },
-    createReaction()
-}
+    createReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body } },
+            { runValidators: true, new: true }
+        )
+            .then((thought) =>
+                !thought ? res.status(404).json({ message: 'No thought was found using this id' })
+                    : res.json(thought))
+            .catch(err => res.status(400).json(err));
+    },
+    deleteReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: { reactionId: req.params.reactionId } } },
+            { runValidators: true, new: true }
+        )
+            .then((thought) =>
+                !thought ? res.status(404).json({ message: 'No thought was found using this id' })
+                    : res.json(thought))
+            .catch(err => res.status(400).json(err));
+    }
+};
