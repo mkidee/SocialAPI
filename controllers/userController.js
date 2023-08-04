c0c95f03a57bab65d6bfa9b2441c4563574adee1
mkidee <mckaymemmott@gmail.com>
2023-08-03 19:00:29 -0600

    add and delete friends

--- controllers/userController.js
+++ controllers/userController.js
@@ -50,6 +50,29 @@ module.exports = {
                 .catch((err) => res.status(400).json(err));
     },
     addFriend(req, res) {
-        
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.params.friendId } },
+            { runValidators: true, new: true}
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user found using this id.' }) 
+                    : res.json(user)
+                )
+                .catch((err) => res.status(400).json(err));
+    },
+    deleteFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: req.params.friendId } },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user found using this id.' })
+                    : res.json(user)
+                )
+                .catch((err) => res.status(400).json(err));
     }
-}
+};
